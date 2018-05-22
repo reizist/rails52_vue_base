7b59774c9ebaa555bc122f237f0a2f003b9d1beb
reizist <reizist@gmail.com>
2018-05-23 03:18:28 +0900

    :lipstick: for eslint

--- app/assets/javascripts/packs/hello_vue.js
+++ app/assets/javascripts/packs/hello_vue.js
@@ -5,10 +5,10 @@
 import Vue from 'vue'
 import App from './app.vue'
 
-document.body.appendChild(document.createElement('hello'))
+document.body.appendChild(document.createElement('hello'));
 
 new Vue({
   el: 'hello',
   template: '<App/>',
-  components: { App }
-})
+  components: { App },
+});
